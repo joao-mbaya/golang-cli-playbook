02bac16986e175492cb96bc90509efd756b78ddd
Joao Mbaya <mbaya19@gmail.com>
2022-02-02 10:28:00 -0500

    module7 task1

--- module6/module6.go
+++ module6/module6.go
@@ -2,4 +2,5 @@ package module6
 
 func FunctionForModule6GoDoc() {
 	//moduel 6 comments
+	//Run go doc module6.go FunctionForModule6GoDoc > module6.txt
 }
--- module7/module7.go
+++ module7/module7.go
@@ -1,3 +1,4 @@
 package module7
 
 //go:generate echo "command and arguments"
+//go:generate command "goimports -w module7_code.go"
